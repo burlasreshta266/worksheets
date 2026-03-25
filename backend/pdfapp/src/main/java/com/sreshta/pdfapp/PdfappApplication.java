8c8dd9138dd97c28c8da145b10b14fc4d0955318
Sreshta <burlasreshta26@gmail.com>
2026-03-25 14:41:03 +0530

    started backend

---
+++ backend/pdfapp/src/main/java/com/sreshta/pdfapp/PdfappApplication.java
@@ -0,0 +1,13 @@
+package com.sreshta.pdfapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfappApplication.class, args);
+	}
+
+}
---
+++ backend/pdfapp/src/test/java/com/sreshta/pdfapp/PdfappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sreshta.pdfapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
